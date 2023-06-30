d1db38e8f845a0bd4cc86cbad94fa267f4420753
Nwanna.Joseph <jnwanna@aimsammi.org>
2023-06-30 20:33:34 +0100

    ggll

--- netlify/functions/sendwelcomemail.js
+++ netlify/functions/sendwelcomemail.js
@@ -1,5 +1,6 @@
 import { Handler } from "@netlify/functions";
 import fetch from "node-fetch";
+import postmark from "postmark"
 
 const handler = async function(event) {
     if (event.body === null) {
@@ -17,20 +18,31 @@ const handler = async function(event) {
 
     //automatically generated snippet from the email preview
     //sends a request to an email handler for a subscribed email
-    await fetch(`https://cosmic-torte-fa1094.netlify.app/.netlify/functions/emails/welcome`, {
-        headers: {
-            "netlify-emails-secret": "",
-        },
-        method: "POST",
-        body: JSON.stringify({
-            from: "gidejo3210@devswp.com",
-            to: "gidejo3210@devswp.com",
-            subject: "You've been subscribed",
-            parameters: {
-                name: "gidejo3210@devswp.com",
-                email: "gidejo3210@devswp.com",
-            },
-        }),
+    // await fetch(`https://cosmic-torte-fa1094.netlify.app/.netlify/functions/emails/welcome`, {
+    //     headers: {
+    //         "netlify-emails-secret": "",
+    //     },
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //         from: "gidejo3210@devswp.com",
+    //         to: "gidejo3210@devswp.com",
+    //         subject: "You've been subscribed",
+    //         parameters: {
+    //             name: "gidejo3210@devswp.com",
+    //             email: "gidejo3210@devswp.com",
+    //         },
+    //     }),
+    // });
+
+    let client = new postmark.ServerClient("fad8a96f-4097-4a86-a57c-39c39d7bc582");
+
+    client.sendEmail({
+        "From": "gidejo3210@devswp.com",
+        "To": "gidejo3210@devswp.com",
+        "Subject": "Hello from Postmark",
+        "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
+        "TextBody": "Hello from Postmark!",
+        "MessageStream": "broadcast"
     });
 
     return {
