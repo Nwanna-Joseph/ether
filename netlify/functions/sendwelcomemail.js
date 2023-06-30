ce9f3fb0b588b15d998f635981b27f79cf1f13bf
Nwanna.Joseph <jnwanna@aimsammi.org>
2023-06-30 22:42:15 +0100

    ggll

--- netlify/functions/sendwelcomemail.js
+++ netlify/functions/sendwelcomemail.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 exports.handler = async function (event, context) {
     // your server-side functionality
     await fetch(`https://api.postmarkapp.com/email`, {
