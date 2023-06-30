d5a2d1b5bef54aae61412a7395354ab8bf33107e
Nwanna.Joseph <jnwanna@aimsammi.org>
2023-06-30 23:17:08 +0100

    build

---
+++ netlify/functions/e.js
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+exports.handler = async function (event, context) {
+    // your server-side functionality
+    const info = await fetch(`${process.env.URL}/.netlify/functions/emails/welcome`, {
+        headers: {
+            "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET as string,
+        },
+        method: "POST",
+        body: JSON.stringify({
+            from: "gidejo3210@devswp.com",
+            to: "gidejo3210@devswp.com",
+            subject: "You've been subscribed",
+            parameters: {
+                name: "Andrechukwu",
+            },
+        }),
+    });
+    const res = await info.json()
+    return {
+        statusCode: 200,
+        body: JSON.stringify(res),
+    };
+};
