c560db1f9f879a23a0a140940d720aa845e0263d
Nwanna.Joseph <jnwanna@aimsammi.org>
2023-06-30 20:39:16 +0100

    ggll

--- netlify/functions/sendwelcomemail.js
+++ netlify/functions/sendwelcomemail.js
@@ -34,16 +34,33 @@ const handler = async function(event) {
     //     }),
     // });
 
-    let client = new postmark.ServerClient("fad8a96f-4097-4a86-a57c-39c39d7bc582");
+    // let client = new postmark.ServerClient("fad8a96f-4097-4a86-a57c-39c39d7bc582");
+    //
+    // client.sendEmail({
+    //     "From": "gidejo3210@devswp.com",
+    //     "To": "gidejo3210@devswp.com",
+    //     "Subject": "Hello from Postmark",
+    //     "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
+    //     "TextBody": "Hello from Postmark!",
+    //     "MessageStream": "broadcast"
+    // });
 
-    client.sendEmail({
-        "From": "gidejo3210@devswp.com",
-        "To": "gidejo3210@devswp.com",
-        "Subject": "Hello from Postmark",
-        "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
-        "TextBody": "Hello from Postmark!",
-        "MessageStream": "broadcast"
+    await fetch(`https://api.postmarkapp.com/email`, {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type":"application/json"
+            "X-Postmark-Server-Token":"fad8a96f-4097-4a86-a57c-39c39d7bc582"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "From": "gidejo3210@devswp.com",
+            "To": "gidejo3210@devswp.com",
+            "Subject": "Hello from Postmark",
+            "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
+            "MessageStream": "broadcast"
+        }),
     });
+
 
     return {
         statusCode: 200,
