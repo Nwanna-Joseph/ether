c39acc45dbf4e46a603394cd89c5f6e1d21851c3
Nwanna.Joseph <jnwanna@aimsammi.org>
2023-07-01 01:19:57 +0100

    added cors

--- netlify/functions/e.js
+++ netlify/functions/e.js
@@ -16,8 +16,22 @@ exports.handler = async function (event, context) {
         }),
     });
     const res = await info.json()
+
+    let HEADERS = {
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
+        'Content-Type': 'application/json', //optional
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Max-Age': '8640'
+    }
+
+//This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
+
+    HEADERS['Access-Control-Allow-Origin'] = '*'
+    HEADERS['Vary'] = 'Origin'
+
     return {
         statusCode: 200,
         body: JSON.stringify(res),
+        HEADERS
     };
 };
