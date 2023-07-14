00a447b31b1897eb59c7c17b72d6d7d420350ed6
Nwanna.Joseph <jnwanna@aimsammi.org>
2023-07-14 12:35:04 +0100

    added test email

---
+++ netlify/functions/emailerdemo.js
@@ -0,0 +1,37 @@
+import fetch from "node-fetch";
+exports.handler = async function (event, context) {
+
+    const {target} = JSON.parse(event.body);
+
+    const info = await fetch(`${process.env.URL}/.netlify/functions/emails/welcome`, {
+        headers: {
+            "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
+        },
+        method: "POST",
+        body: JSON.stringify({
+            from: `gidejo3210@devswp.com`,
+            to: `gidejo3210@devswp.com` ,
+            subject: `Welcome ${target}`, //sanitize for xss
+            parameters: {
+                name:`${target}`, //sanitize for xss
+            },
+        }),
+    });
+    
+    const res = await info.json()
+
+    let HEADERS = {
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
+        'Content-Type': 'application/json', //optional
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Max-Age': '8640'
+        'Access-Control-Allow-Origin': "*",
+        'Vary': "Origin"
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(res),
+        HEADERS
+    };
+};
